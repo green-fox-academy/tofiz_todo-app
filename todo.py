edea602c1caa2bfdd9aaccdb5010b851937cbdd0
Tofiz <tofi.zagon@gmail.com>
2017-09-28 17:44:05 +0200

    feat(todo.py): selector and list_tasks functions created

--- todo.py
+++ todo.py
@@ -7,19 +7,36 @@ print("Command Line Todo application \n=============================\n \n"
         "Command line arguments: \n -l   Lists all the tasks \n" ,
         "-a   Adds a new task \n -r   Removes an task \n -c   Completes an task")
 
-
-
-
 def argument_reader(arg):
     if arg == "l":
-        return list_of_tasks()
+        return convert_txt_to_data()
     else:
         print("Hey, use the given arguments")
 
-def list_of_tasks():
-    # tasks = open("tasks.txt", "r")
-    tasks = [{'checked',"learn to code"}, {'unchecked',"read the styleguide"}, {'unchecked',"watch the news"}]
-    for i in range(len(tasks)):
-        print(tasks[i])
+def convert_txt_to_data():
+    f = open("tasks.txt", "r")
+    full_text = f.readlines()
+    task = []
+    for line in full_text:
+        task.append(line)
+    return task
 
-argument_reader(argument[1])
+def done_or_upcoming_selector(task):
+    dones = ""
+    upcomings = ""
+    for element in task:
+        if element[:1] == "1":
+            dones += element[2:]
+            
+        elif element[:1] == "0":
+            upcomings += element[2:]
+            
+    return dones, upcomings
+
+   
+def list_tasks(d, u):
+    print(u)
+
+dones, upcomings = done_or_upcoming_selector(argument_reader(argument[1]))
+task = convert_txt_to_data()
+list_tasks(dones, upcomings)
